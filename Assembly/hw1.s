ee08277232dced58ee113ae9f90fc04b97240ce3
Davis Chou <lxdfs58@gmail.com>
2022-01-13 15:17:38 +0800

    HW1_assembly

--- Assembly/hw1.s
+++ Assembly/hw1.s
@@ -6,7 +6,33 @@
 
 FUNCTION:
     # Todo: Define your own function in HW1
-    
+    addi t0, x0, 0
+    add t0, t0, a0
+    jal x1, recursive
+    jal x0, result
+recursive:  
+    # Save return address and n on stack
+    addi sp, sp, -8
+    sw x1, 4(sp)
+    sw a0, 0(sp)
+
+    addi t2, x0, 1# Set tem2 to 1
+    bgt a0, t2, L1# If n > tem2, go to L1
+    addi t0, x0, 4# Else, set T(1) to 4 
+    addi sp, sp, 8# Pop stack
+    jalr x0, 0(x1)# Return
+
+L1:
+    srli a0, a0, 1# n = n/2
+    jal x1, recursive# Call recursive T(n/2)
+    lw a0, 0(sp)# restore caller's n
+    lw x1, 4(sp)# restore caller's return address
+    addi sp, sp, 8# Pop stack
+    slli t0, t0, 1# T(n/2) = T(n/2) * 2
+    slli x20, a0, 3# tem4 = n * 8
+    addi x20, x20, 5# tem4 = tem4 + 5
+    add t0, t0, x20# T(n) = 2T(n/2) + 8n + 5
+    jalr x0, 0(x1)# Retrun
 
 # Do NOT modify this part!!!
 __start:
